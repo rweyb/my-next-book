a17219f89f578db4a1a181a665be0c57407d7af0
rweyb <danyulingnai8@gmail.com>
2024-09-06 14:12:34 +0900

    ポートフォリオ完成

--- .history/src/state/favoritesState_20240904180035.js
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-
-// お気に入りの状態を管理
-export const favoritesState = atom({
-  key: 'favoritesState',
-  default: [], // デフォルト値は空の配列
-});
--- .history/src/state/recommendationsState_20240905113805.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const recommendationsState = atom({
-  key: "recommendationsState",
-  default: [], // 初期値として空の配列
-});
